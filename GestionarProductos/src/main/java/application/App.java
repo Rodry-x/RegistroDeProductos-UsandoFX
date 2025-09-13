abf2b48cc2270042dadb5c8d926133cec3e2f129
Rodry-x <166262311+Rodry-x@users.noreply.github.com>
2025-09-13 12:45:06 -0500

    Add files via upload

---
+++ GestionarProductos/src/main/java/application/App.java
@@ -0,0 +1,15 @@
+package application;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage){
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
